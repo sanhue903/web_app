bc39651456b38d6e856454ab0c214b79f77c66ed
Pedro Muñoz <pemunoz2021@udec.cl>
2024-06-12 03:03:04 -0400

    primera vista de login

---
+++ frontend/aulas/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+      <h1 class="titulo">Bienvenid@ al botiquín de las emociones</h1>
+
+      <div class="login-form">
+        <h2>Inicio de sesión</h2>
+        <input type="text" placeholder="Usuario" />
+        <input type="password" placeholder="Contraseña" />
+        <button>Ingresar</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
